a0c7ba174f81887544312f3239702041f89988f4
Joris Engbers <info@jorisengbers.nl>
2016-08-18 16:04:26 +0200

    Use MariaDB, Python3 and latest Django.
    All versioning has been removed. The benefit is that you will always be
    running the latest and greatest of everything from the start. Because
    the container django:latest is pretty much guaranteed to work the risk
    that this will ever not work is very small.
    
    Django extensions and ipython have been added to make development
    easier.

--- Dockers/db/create_db_with_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER {{project_name}};
-CREATE DATABASE {{project_name}};
-GRANT ALL PRIVILEGES ON DATABASE {{project_name}} TO {{project_name}};
-ALTER USER {{project_name}} CREATEDB;
--- Dockers/db/init_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Grant {{project_name}} access from all IPs."
-echo "host all {{project_name}} 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf
-
-echo "Done initializing."
